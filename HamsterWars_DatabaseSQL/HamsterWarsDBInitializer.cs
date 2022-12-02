78098d60ea2951ccdc46122b24390c2834af0144
Markus Nordin <belfegorc4@gmail.com>
2022-12-02 18:17:45 +0100

    dswqd

---
+++ HamsterWars_DatabaseSQL/HamsterWarsDBInitializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HamsterWars_DatabaseSQL.Models;
+
+namespace HamsterWars_DatabaseSQL
+{
+    public class HamsterWarsDBInitializer : CreateDatabaseIfNotExists<HamsterContext>
+    {
+        protected override void Seed(HamsterContext context)
+        {
+            IList<Hamster> defaultStandards = new List<Hamster>();
+            base.Seed(context);
+        }
+    }
+}
