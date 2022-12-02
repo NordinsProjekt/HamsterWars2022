f50a34f824f56c64067d984f6b90218789c7ece1
Markus Nordin <belfegorc4@gmail.com>
2022-12-02 23:52:25 +0100

    Repository pattern

---
+++ HamsterWars_DatabaseSQL/DAL/IMatchRepository.cs
@@ -0,0 +1,20 @@
+﻿using HamsterWars_DatabaseSQL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HamsterWars_DatabaseSQL.DAL
+{
+    public interface IMatchRepository
+    {
+        IEnumerable<Match> GetMatches();
+        Match GetMatchByID(int matchId);
+        object InsertMatch(Match match);
+        bool DeleteMatch(int matchId);
+        Match[] GetAllWonMatches(int hamsterId);
+        Hamster[] GetTop5WinnerHamsters();
+        Hamster[] GetTop5LoserHamsters();
+    }
+}
