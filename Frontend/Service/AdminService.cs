503555ff9dc25f30d8ad6c96ce857b6cbd3c5fee
Markus Nordin <82381561+NordinsProjekt@users.noreply.github.com>
2022-12-28 09:43:15 +0100

    g5g5g5

---
+++ Frontend/Service/AdminService.cs
@@ -0,0 +1,25 @@
+﻿using Frontend.DTO;
+namespace Frontend.Service
+{
+    public class AdminService
+    {
+        private string _api;
+        private HttpClient _httpClient = new HttpClient();
+        public AdminService(string apiRoute)
+        { 
+            _api = apiRoute;
+            _httpClient.BaseAddress = new Uri(_api);
+        }
+
+        public async Task<bool> VoteInBattle(int[] arr)
+        {
+            VoteDTO vote = new VoteDTO(arr[0], arr[1]);
+            await _httpClient.PostAsJsonAsync("vote", vote);
+            await _httpClient.PostAsJsonAsync("EndMatch", arr[0]);
+            _httpClient.Dispose();
+            //await JS.InvokeVoidAsync("postAPI", "https://localhost:7101/vote/", vote);
+            //await JS.InvokeVoidAsync("postAPI", "https://localhost:7101/EndMatch/" + matchVote[0]);
+            return true;
+        }
+    }
+}
